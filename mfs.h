7d222e96bc7e73e5c839e449eca9aac006147668
Student <tfulinara@sfsu.edu>
2023-07-13 08:30:15 -0700

    started work on file system interface header file

--- mfs.h
+++ mfs.h
@@ -1,16 +1,16 @@
 /**************************************************************
-* Class:  CSC-415
-* Name: Professor Bierman
-* Student ID: N/A
+* Class:  CSC-415-01 Summer 2023
+* Names: Tyler Fulinara, Rafael Sant Ana Leitao, Anthony Silva , Vinh Ngo Rafael Fabiani
+* Student IDs: 922002234, 920984945,
+922907645, 921919541,
+922965105
+* GitHub Name: rf922
+* Group Name: MKFS
 * Project: Basic File System
 *
 * File: mfs.h
 *
-* Description: 
-*	This is the file system interface.
-*	This is the interface needed by the driver to interact with
-*	your filesystem.
-*
+* Description: File for handling
 **************************************************************/
 #ifndef _MFS_H
 #define _MFS_H
@@ -18,9 +18,12 @@
 #include <unistd.h>
 #include <time.h>
 
-#include "b_io.h"
-
 #include <dirent.h>
+#include "b_io.h"
+#include "fsLow.h"
+#include "root_init.h"
+#include "vcb_.h"
+
 #define FT_REGFILE	DT_REG
 #define FT_DIRECTORY DT_DIR
 #define FT_LINK	DT_LNK
@@ -71,6 +74,33 @@ int fs_isFile(char * filename);	//return 1 if file, 0 otherwise
 int fs_isDir(char * pathname);		//return 1 if directory, 0 otherwise
 int fs_delete(char* filename);	//removes a file
 
+
+
+// extra handlers 
+void build_absolute_path(char * buf, const char * pathname);
+
+// This function checks whether the given attribute represents a directory.
+// It takes an attribute as input and returns 1 if the attribute 
+// indicates a directory, and 0 otherwise.
+int check_directory_attribute(int attribute);
+
+
+// This function searches for a directory entry that matches the provided token (directory name)
+// within the given array of directory entries (current_dir_ent).
+// It takes the current directory entry, the token to match, the current path index, and the total path length as input.
+// If a matching directory entry is found, it allocates memory for a new directory entry,
+// copies the matching entry into it, and returns the new directory entry.
+// If no match is found, it returns NULL.
+Directory_Entry* get_target_directory(Directory_Entry* current_dir_ent, char* token, int pathIndex, int pathLength);
+
+
+// This function parses a directory path and finds the corresponding directory entry.
+// It takes the path and the target parameter as input.
+// If the provided path is NULL, the function prints an error message and returns NULL.
+// Otherwise, it searches for the directory entry that matches the path.
+// If a matching directory entry is found, it returns a pointer to the directory entry.
+// If no matching directory entry is found or an error occurs, it returns NULL.
+Directory_Entry* parse_directory_path(const char* path, int target);
 
 // This is the strucutre that is filled in from a call to fs_stat
 struct fs_stat
