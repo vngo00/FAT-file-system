9e7f7d55c325e0af4c8a87ce1ddeb2ca0dcd59f6
Rafael <rafael.s.leitao@gmail.com>
2023-07-25 13:03:19 -0700

    Comments added to b_io.h

--- b_io.h
+++ b_io.h
@@ -19,10 +19,25 @@
 
 typedef int b_io_fd;
 
+// Opens a file with the given filename and flags. 
+// Returns a file descriptor.
 b_io_fd b_open (char * filename, int flags);
+
+// Reads count bytes from the file described by fd into the buffer. 
+// Returns the number of bytes read.
 int b_read (b_io_fd fd, char * buffer, int count);
+
+// Writes count bytes from the buffer into the file described by fd.
+// Returns the number of bytes written.
 int b_write (b_io_fd fd, char * buffer, int count);
+
+// It moves the file offset of the file description indicated by fd 
+// by offset bytes according to whence.
+// Returns the resulting offset location in bytes from the beginning of the file.
 int b_seek (b_io_fd fd, off_t offset, int whence);
+
+// Closes the file descriptor fd.
+// Returns zero on success, or -1 if error.
 int b_close (b_io_fd fd);
 
 #endif
